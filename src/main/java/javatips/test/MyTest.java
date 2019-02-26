ba1ce3e801d81ea1e31d967482c8731bdb5c6bec
churchTao <taojiacheng@19lou.com>
2019-02-26 13:58:02 +0800

    原版 报名页

--- src/main/java/javatips/test/MyTest.java
+++ src/main/java/javatips/test/MyTest.java
@@ -1,8 +1,10 @@
 package javatips.test;
 
+import java.util.stream.Stream;
+
 public class MyTest {
 
     public static void main(String[] args) {
-
+        Stream.iterate(0,n -> n+n+1).limit(10).forEach(System.out::println);
     }
 }
